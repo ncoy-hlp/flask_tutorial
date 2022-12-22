75ff706fc9119b015496a5e77059f7da58623f96
ncoy-hlp <ncoy@handlparnters.com>
2022-12-21 17:00:28 -0800

    adding updates for user login system

--- app/flask_blog/__init__.py
+++ app/flask_blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # this is for development
 app = Flask(__name__)
@@ -8,6 +9,10 @@ app.config["SECRET_KEY"] = '547d656f1f3e3983b1a27d7bedd40975'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 # import after app initialization 
 # otherwise there is a circular import
