01b56440b0f6e815850098ab30169c4733941d7f
ncoy-hlp <ncoy@handlparnters.com>
2022-12-29 13:22:48 -0800

    changing project structure

---
+++ app/flask_blog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("FLASK_DATABASE_URI")
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("FLASK_EMAIL")
+    MAIL_PASSWORD = os.environ.get("FLASK_EMAIL_PASSWORD")
