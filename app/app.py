eeff87d94530dc5dff9fd67f0ff9bc1115c6bb60
ncoy-hlp <ncoy@handlparnters.com>
2022-12-31 12:03:19 -0800

    changing where db create all is run

--- app/app.py
+++ app/app.py
@@ -1,8 +1,3 @@
-from flask_blog import create_app, db
+from flask_blog import create_app
 
 app = create_app()
-with app.app_context():
-    db.create_all()
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0")
